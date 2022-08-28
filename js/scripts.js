1e78d5593f30176cb5fa706a10c2b0b2d6d48134
RemyXVX <remyfranciscoflores@gmail.com>
2022-08-28 02:16:04 -0700

    Still working on fixing my submit issue, will be troubleshooting

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,40 @@
 window.addEventListener("load", function() {
 
-  const form = document.querySelector("form");
-  let resetBtn = document.querySelector("reset");
-  let answer = document.querySelector("answer");
+  let form = document.querySelector("form");
+  let resetBtn = document.getElementById("reset");
+  let answer = document.getElementById("answer");
 
   form.addEventListener("submit", function(event) {
-    const question1 = document.getElementById("question1").value;
-    const question2 = document.getElementById("question2").value;
-    const question3 = document.getElementById("question3").value;
-    const question4 = document.getElementById("question4").value;
-    const question5 = document.getElementById("question5").value;
 
-    document.querySelector("")
+    const question1 = parseInt(document.querySelector("#input[name='question']:checked").value);
+    const question2 = parseInt(document.querySelector("#input[name='questionA']:checked").value);
+    const question3 = parseInt(document.querySelector("#input[name='questionB']:checked").value);
+    const question4 = parseInt(document.querySelector("#input[name='questionC']:checked").value);
+    const question5 = parseInt(document.querySelector("#input[name='questionD']:checked").value);
+
+    let result = question1 + question2 + question3 + question4 + question5
+
+    if (result === 1) {
+      document.getElementById("resultJavascript");
+    } else if (result === 2) {
+      document.getElementById("resultPython");
+    } else if (result === 3) {
+      document.getElementById("resultC");
+    } else if (result === 4) {
+      document.getElementById("resultRuby");
+    } else if (result === 5) {
+      document.getElementById("resultSwift");
+
+    result.removeAttribute("class");
+    event.preventDefault();
+    };
   });
 
-  /*form.addEventListener("reset")*/
+  form.addEventListener("submit", function() {
+    resetBtn.removeAttribute("class");
+  });
 
-  resetBtn.addEventListener("click", function() {
+  resetBtn.addEventListener("reset", function() {
     answer.setAttribute("class", "hidden");
     document.getElementById("question1").value = null;
     document.getElementById("question2").value = null;
